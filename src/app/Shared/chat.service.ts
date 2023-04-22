2c8567a6c7cb743a32a87c7350ac69e2cccd7351
zackdarklord <zakaria.chelbi@esprit.tn>
2023-04-22 06:00:31 +0100

    loadContacts/connect SockJs/SendMessage

--- src/app/Shared/chat.service.ts
+++ src/app/Shared/chat.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import * as SockJS from 'sockjs-client';
+import * as Stomp from 'stompjs-websocket';
+import { EncryptionService } from './encryption.service';
+
+
 //import { ChatMessage } from '../models/chat-message.model';
 
 @Injectable({
@@ -10,8 +15,44 @@ export class ChatService {
 
   private baseUrl = 'http://localhost:8082';
 
-  constructor(private http: HttpClient) { }
+  private socket: any;
+  data: any;
 
+  constructor(private http: HttpClient, private encryptionService: EncryptionService) {
+    this.data = this.encryptionService.decrypt(localStorage.getItem('data')!);
+    console.log(this.data);
+    const socket = new SockJS('http://localhost:8082/ws');
+    this.connect();
+  }
+  public stompClient: any;
+  public msg: any;
+  connect() {
+    const socket = new SockJS('http://localhost:8082/ws');
+    this.stompClient = Stomp.over(socket);
+
+    // Add JWT token to the header
+    const jwtToken = this.data['token'];
+    const headers = {
+      Authorization: `${jwtToken}`
+    };
+    console.log(headers);
+
+    this.stompClient.connect(headers, this.onConnect.bind(this), this.onError.bind(this));
+  }
+
+  onConnect(frame: any) {
+    
+    console.log("connected" + this.data);
+
+    this.stompClient.subscribe(
+      "/user/" + this.data['id'] + "/queue/messages" //change id to userName 
+    );
+    // this.sendMessage("zakaria aadzqdqslcsqcqs");
+  }
+
+  onError(error: any) {
+    console.log(error);
+  }
   processMessage(body: any): void {
     this.http.post(`${this.baseUrl}/chat`, body).subscribe();
   }
@@ -39,4 +80,15 @@ export class ChatService {
   addMessage(body: any): Observable<any> {
     return this.http.post<any>(`${this.baseUrl}/chat/add`, body);
   }
+
+  sendMessage(message: any) {
+
+
+    this.stompClient.send("/app/chat", {}, JSON.stringify(message));
+
+  }
+  public getuserActive() {
+    return this.http.get(this.baseUrl + "/getUser");
+  }
+
 }
--- src/app/chat/display-chat/display-chat.component.ts
+++ src/app/chat/display-chat/display-chat.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ChatService } from '../../Shared/chat.service';
+import { EncryptionService } from 'src/app/Shared/encryption.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -8,10 +10,46 @@ import { ChatService } from '../../Shared/chat.service';
   templateUrl: './display-chat.component.html',
   styleUrls: ['./display-chat.component.css']
 })
-export class DisplayChatComponent  {
+export class DisplayChatComponent  implements OnInit{
+ data:any[]=[];
+ input:any;
+ data1:any;
 
+  MessageAdd = new FormGroup({
+  recipientId: new FormControl('slimayadi'),
+  senderId: new FormControl('zakaria'),
+  senderName: new FormControl('zakaria'),
+  recipientName: new FormControl('slimayadi'),
+  content: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(20)]),
+  timestamp: new FormControl(new Date().toISOString())
+});
 
-  constructor() { }
+  constructor(private encrypt: EncryptionService,private chatService:ChatService ) {}
+  sendMessage() {
+    
+      this.chatService.sendMessage(this.MessageAdd.value)
+
+     
+    }
+ 
+
+  getAciveUsers (){
+    this.chatService.getuserActive().subscribe((res:any)=>{
+      
+      this.data=res;
+      console.log(res);
+    });
+
+  }
+ ngOnInit(): void {
+   this.getAciveUsers();
+   this.data1 = this.encrypt.decrypt(localStorage.getItem('data')!);
+   this.MessageAdd.get("recipientId")!.setValue("zakaria");
+   this.MessageAdd.get("senderId")!.setValue(this.data1['id']);
+   this.MessageAdd.get("senderName")!.setValue(this.data1['id']);
+   this.MessageAdd.get("recipientName")!.setValue("zakaria");
+   
+ }
 
  
 }
