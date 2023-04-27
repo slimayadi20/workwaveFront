fa915ec92cf7515c86c940c7c3a7e576d3474fc7
Wassim Ghiloufi <48228694+Ghiloufi1998@users.noreply.github.com>
2023-04-27 04:40:11 +0100

    fix

--- src/app/bank-accounts/bank-accounts.module.ts
+++ src/app/bank-accounts/bank-accounts.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BankAccountsRoutingModule } from './bank-accounts-routing.module';
-import { ListAccountsComponent } from './list-accounts/list-accounts.component';
-import { EditAccountsComponent } from './edit-accounts/edit-accounts.component';
 import { ViewmybankaccountComponent } from './viewmybankaccount/viewmybankaccount.component';
 import { ReactiveFormsModule  } from '@angular/forms';
 import { NgxPayPalModule } from 'ngx-paypal';
@@ -13,8 +11,6 @@ import { AddbanaccountComponent } from './addbanaccount/addbanaccount.component'
 
 @NgModule({
   declarations: [
-    ListAccountsComponent,
-    EditAccountsComponent,
     ViewmybankaccountComponent,
     AddbanaccountComponent
   ],
