aeda734dc1d6c2445924feae5f0fa96b634f0fc6
Wassim Ghiloufi <ghiloufiwassim@gmail.com>
2023-05-05 15:48:13 +0100

    push before merge with slim

--- src/app/dashboard-component/dashboard-component.component.ts
+++ src/app/dashboard-component/dashboard-component.component.ts
@@ -1,12 +1,20 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { NgProgressComponent } from 'ngx-progressbar';
+import { AuthServiceService } from '../Shared/auth-service.service';
 
 @Component({
   selector: 'app-dashboard-component',
   templateUrl: './dashboard-component.component.html',
   styleUrls: ['./dashboard-component.component.css']
 })
-export class DashboardComponentComponent implements AfterViewInit {
+export class DashboardComponentComponent implements AfterViewInit, OnInit {
+  roles: any = [];
+  constructor(private auth :AuthServiceService) { }
+  ngOnInit(): void {
+    this.roles=this.auth.getroles
+    
+  }
+
 
   @ViewChild(NgProgressComponent) progressBar!: NgProgressComponent;
 
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthServiceService } from '../Shared/auth-service.service';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
-  constructor() { }
+export class MainComponent implements OnInit {
+  constructor(private auth :AuthServiceService) { }
+  ngOnInit(): void {
+    
+  }
+
+
 
 }
